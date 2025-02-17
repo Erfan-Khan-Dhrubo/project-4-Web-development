f4b18f79b88a65fea106a42864446e05441f342c
Erfan Khan <erfan.khan.cse@gmail.com>
2025-02-17 21:46:21 +0600

    1st task

---
+++ task_1.js
@@ -0,0 +1,15 @@
+function cashOut(money) {
+  if (typeof money == "number") {
+    if (money >= 0) {
+      let cashOutCharge = money * (1.75 / 100);
+      return cashOutCharge;
+    } else {
+      return "Invalid";
+    }
+  } else {
+    return "Invalid";
+  }
+}
+
+let a = cashOut(2000);
+console.log(a);
